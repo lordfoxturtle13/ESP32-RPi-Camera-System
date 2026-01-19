f7f4ef9964f2cd82c46f9eee3e6a5e0d47cef917
Teker Balázs <lordfoxturtle13@gmail.com>
2026-01-20 00:51:44 +0100

    rendbe lett rakva a mac-en is a fájlrendszer

--- esp32_cam/src/CameraWebServer_Proba01.ino
+++ esp32_cam/src/CameraWebServer_Proba01.ino
@@ -74,7 +74,7 @@ void setup() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
 
-  config.xclk_freq_hz = 10000000; //20000000-ről állítottam át a felére, ez jobbnak bizonyult eddig
+  config.xclk_freq_hz = 20000000; //20000000-ről állítottam át a felére, ez jobbnak bizonyult eddig
   config.frame_size = FRAMESIZE_SVGA; //FRAMESIZE_UXGA;
   config.pixel_format = PIXFORMAT_JPEG;  // for streaming
   //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
